532b698b5afa08a0f65be0b00e2f961ed21a7e56
Венцислав <ventsislav.varbanov@gmail.com>
2017-04-24 23:53:24 +0300

    Update dmv_missing_indexes.sql

--- dmv_missing_indexes.sql
+++ dmv_missing_indexes.sql
@@ -1,5 +1,13 @@
 --SQL Server 2012/2014
-select gs.user_seeks, gs.user_scans, gs.last_user_seek, gs.last_user_scan, gs.unique_compiles, id.equality_columns,  id.[statement]
+select 
+gs.user_seeks, 
+gs.user_scans, 
+gs.last_user_seek, 
+gs.last_user_scan, 
+gs.unique_compiles, 
+db_name(id.database_id) 'database', 
+id.equality_columns,  
+id.[statement]
 from sys.dm_db_missing_index_group_stats gs
 join sys.dm_db_missing_index_groups g
 on g.index_group_handle = gs.group_handle
